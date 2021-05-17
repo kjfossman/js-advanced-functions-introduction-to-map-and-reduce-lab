82339694fdce59d07c44720ce58bf699b54302d9
Kyle <kjfossman@gmail.com>
2021-05-17 14:52:14 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Your code here
+function mapToNegativize(array){
+    let newA = array.map(element => element * -1)
+    return newA
+}
+
+function mapToNoChange(array){
+    return array.map(e => e)
+}
+
+function mapToDouble(array){
+    return array.map(e => e*2)
+}
+
+function mapToSquare(array){
+    return array.map(e => e*e)
+}
+
+function reduceToTotal(array, start = 0){
+    const reducer = function(start, number){number + start}
+    return array.reduce(reducer, start)
+}
